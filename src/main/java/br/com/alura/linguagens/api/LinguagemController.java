069ad0824b53a514c965fc9f8b5f3b5c407a0b4e
Sants16 <jvsantosrocha980@gmail.com>
2023-03-30 16:56:50 -0300

    first commit

---
+++ src/main/java/br/com/alura/linguagens/api/LinguagemController.java
@@ -0,0 +1,58 @@
+package br.com.alura.linguagens.api;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+public class LinguagemController {
+
+    @Autowired
+    private LinguagemRepository repositorio;
+
+    @GetMapping("/linguagens")
+    public List<Linguagem> obterLinguagens(){
+        return repositorio.findByOrderByRanking();
+    }
+
+    @PostMapping("/linguagens")
+    public ResponseEntity<Linguagem> cadastrarLinguagem(@RequestBody Linguagem linguagem){
+        Linguagem linguagemSalva = repositorio.save(linguagem);
+        return new ResponseEntity<>(linguagemSalva, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/linguagens/{id}")
+    public Linguagem obterLinguagemPorId(@PathVariable String id){
+        return repositorio.findById(id)
+            .orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND)
+            );
+    }
+
+    @PutMapping("/linguagens/{id}")
+    public Linguagem atualizarLinguagem(@PathVariable String id, @RequestBody Linguagem linguagem){
+        if(!repositorio.existsById(id)){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        
+        linguagem.setId(id);
+        Linguagem linguagemSalva = repositorio.save(linguagem);
+        return linguagemSalva;
+    }
+
+    @DeleteMapping("/linguagens/{id}")
+    public void excluirLinguagem(@PathVariable String id){
+        repositorio.deleteById(id);
+    }
+
+}
